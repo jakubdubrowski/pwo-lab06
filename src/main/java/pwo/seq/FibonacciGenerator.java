df2e66be6758fdf849d9dc176c13d23379080314
jakubdubrowski <jakub.dubro@gmail.com>
2022-10-22 11:54:34 +0200

    Dystrybucja 1 optymalizacja wstecz

--- src/main/java/pwo/seq/FibonacciGenerator.java
+++ src/main/java/pwo/seq/FibonacciGenerator.java
@@ -15,6 +15,20 @@ public class FibonacciGenerator extends Generator {
 
     }
 
+    static void FibonacciGeneratorOpt(int n) {
+        int a[] = new int[n];
+        a[0] = 0;
+        a[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            a[i] = a[i - 2] + a[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
     @Override
     public void reset() {
         super.reset();
